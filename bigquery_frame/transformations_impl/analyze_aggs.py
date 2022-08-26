529494e73f5237b8f0dc069e564d7290d5962462
fpin <pin.furcy@gmail.com>
2022-08-26 15:51:59 +0200

    feat!: improve the analyze() transformation
    The method now return a new column by default: column_number.
    Custom aggregations can now be specified, and stats can now
    be grouped against one or several columns.

---
+++ bigquery_frame/transformations_impl/analyze_aggs.py
@@ -0,0 +1,41 @@
+from google.cloud.bigquery import SchemaField
+
+from bigquery_frame import functions as f
+from bigquery_frame.column import Column
+
+
+def column_number(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.lit(col_num).alias("column_number")
+
+
+def column_name(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.lit(schema_field.name).alias("column_name")
+
+
+def column_type(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.lit(schema_field.field_type).alias("column_type")
+
+
+def count(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.count(f.lit(1)).alias("count")
+
+
+def count_distinct(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.count_distinct(col).alias("count_distinct")
+
+
+def count_null(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return (f.count(f.lit(1)) - f.count(col)).alias("count_null")
+
+
+def min(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.min(col).cast("STRING").alias("min")
+
+
+def max(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    return f.max(col).cast("STRING").alias("max")
+
+
+def approx_top_100(col: str, schema_field: SchemaField, col_num: int) -> Column:
+    col = f.coalesce(f.col(col).asType("string"), f.lit("NULL"))
+    return f.expr(f"APPROX_TOP_COUNT({col.expr}, 100)").alias("approx_top_100")
