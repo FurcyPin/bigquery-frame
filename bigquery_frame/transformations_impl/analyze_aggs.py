3afd509d1ed50ea190b5ba31bb4b2bd652c5171e
fpin <pin.furcy@gmail.com>
2022-08-26 16:24:27 +0200

    chore: disable Sonar on bigquery_frame.transformations_impl.analyze_aggs
    Sonar keeps complaining about unused method parameter.
    I want those methods to have this signature, for clarity.
    I don't want to make an interface and one static class for each method.
    Thank you.

--- bigquery_frame/transformations_impl/analyze_aggs.py
+++ bigquery_frame/transformations_impl/analyze_aggs.py
@@ -4,38 +4,38 @@ from bigquery_frame import functions as f
 from bigquery_frame.column import Column
 
 
-def column_number(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def column_number(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.lit(col_num).alias("column_number")
 
 
-def column_name(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def column_name(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.lit(schema_field.name).alias("column_name")
 
 
-def column_type(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def column_type(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.lit(schema_field.field_type).alias("column_type")
 
 
-def count(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def count(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.count(f.lit(1)).alias("count")
 
 
-def count_distinct(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def count_distinct(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.count_distinct(col).alias("count_distinct")
 
 
-def count_null(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def count_null(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return (f.count(f.lit(1)) - f.count(col)).alias("count_null")
 
 
-def min(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def min(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.min(col).cast("STRING").alias("min")
 
 
-def max(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def max(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     return f.max(col).cast("STRING").alias("max")
 
 
-def approx_top_100(col: str, schema_field: SchemaField, col_num: int) -> Column:
+def approx_top_100(col: str, schema_field: SchemaField, col_num: int) -> Column:  # NOSONAR
     col = f.coalesce(f.col(col).asType("string"), f.lit("NULL"))
     return f.expr(f"APPROX_TOP_COUNT({col.expr}, 100)").alias("approx_top_100")
