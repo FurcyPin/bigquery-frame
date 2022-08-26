b172106cace6e5991dad1c7262c86019f889a7f6
fpin <pin.furcy@gmail.com>
2022-08-26 15:51:59 +0200

    feat!: improve the analyze() transformation
    The method now return a new column by default: column_number.
    Custom aggregations can now be specified, and stats can now
    be grouped against one or several columns.

--- bigquery_frame/transformations_impl/analyze.py
+++ bigquery_frame/transformations_impl/analyze.py
@@ -1,17 +1,19 @@
-from typing import List
+from typing import Callable, List, Optional, Union
 
 from google.cloud.bigquery import SchemaField
 
 from bigquery_frame import BigQueryBuilder, DataFrame
 from bigquery_frame import functions as f
 from bigquery_frame.auth import get_bq_client
+from bigquery_frame.column import StringOrColumn, cols_to_str
 from bigquery_frame.dataframe import strip_margin
+from bigquery_frame.transformations_impl import analyze_aggs
 from bigquery_frame.transformations_impl.flatten import flatten_schema
 from bigquery_frame.transformations_impl.union_dataframes import union_dataframes
 from bigquery_frame.utils import quote
 
 
-def _unnest_column(df: DataFrame, col: str):
+def _unnest_column(df: DataFrame, col: str, extra_cols: Optional[List[str]] = None):
     """Recursively unnest a :class:`DataFrame`'s column
 
     >>> bq = BigQueryBuilder(get_bq_client())
@@ -61,6 +63,8 @@ def _unnest_column(df: DataFrame, col: str):
     :param col:
     :return:
     """
+    if extra_cols is None:
+        extra_cols = []
 
     def build_cross_join_statement(split: List[str]):
         previous = ""
@@ -89,7 +93,7 @@ def _unnest_column(df: DataFrame, col: str):
         query = strip_margin(
             f"""
             |SELECT
-            |  {col}
+            |  {cols_to_str(extra_cols + [col], 2)}
             |FROM {quote(df._alias)}
             |{cross_join_str}"""
         )
@@ -98,25 +102,39 @@ def _unnest_column(df: DataFrame, col: str):
         return df
 
 
-def _analyze_column(df: DataFrame, schema_field: SchemaField, col_num: int):
+def _select_group_by(
+    df: DataFrame, *columns: Union[List[StringOrColumn], StringOrColumn], group_by: List[str]
+) -> "DataFrame":
+    """Projects a set of expressions and returns a new :class:`DataFrame`."""
+    if isinstance(columns[0], list):
+        if len(columns) == 1:
+            columns = columns[0]
+        else:
+            raise TypeError(f"Wrong argument type: {type(columns)}")
+    group_by_str = ""
+    if len(group_by) > 0:
+        group_by_str = f"\nGROUP BY {cols_to_str(group_by)}"
+    query = strip_margin(
+        f"""SELECT
+        |{cols_to_str(columns, 2)}
+        |FROM {quote(df._alias)}{group_by_str}"""
+    )
+    return df._apply_query(query)
+
+
+def _analyze_column(df: DataFrame, schema_field: SchemaField, col_num: int, group_by: List[str], aggs: List[Callable]):
     col = schema_field.name
     if "!" in col:
         if col[-1] == "!":
             col = col.split("!")[-2]
         else:
             col = col.split(".")[-1]
-    df = _unnest_column(df, schema_field.name)
-    res = df.select(
-        f.lit(col_num).alias("column_number"),
-        f.lit(schema_field.name).alias("column_name"),
-        f.lit(schema_field.field_type).alias("column_type"),
-        f.count(f.lit(1)).alias("count"),
-        f.count_distinct(col).alias("count_distinct"),
-        (f.count(f.lit(1)) - f.count(col)).alias("count_null"),
-        f.min(col).cast("STRING").alias("min"),
-        f.max(col).cast("STRING").alias("max"),
-        f.expr(f"APPROX_TOP_COUNT(COALESCE(CAST({f.col(col)} as STRING), 'NULL'), 100)").alias("approx_top_100"),
-    )
+    df = _unnest_column(df, schema_field.name, extra_cols=group_by)
+    group_select = []
+    if len(group_by) > 0:
+        group_select = [f.struct(*group_by).alias(quote("group"))]
+
+    res = _select_group_by(df, *group_select, *[agg(col, schema_field, col_num) for agg in aggs], group_by=group_by)
     return res
 
 
@@ -128,11 +146,31 @@ def __chunks(lst: List, n: int):
         # fmt: on
 
 
-def analyze(df: DataFrame, _chunk_size: int = 50):
+default_aggs = [
+    analyze_aggs.column_number,
+    analyze_aggs.column_name,
+    analyze_aggs.column_type,
+    analyze_aggs.count,
+    analyze_aggs.count_distinct,
+    analyze_aggs.count_null,
+    analyze_aggs.min,
+    analyze_aggs.max,
+    analyze_aggs.approx_top_100,
+]
+
+
+def analyze(
+    df: DataFrame,
+    group_by: Optional[Union[str, List[str]]] = None,
+    _aggs: Optional[List[Callable]] = None,
+    _chunk_size: int = 50,
+):
     """Analyze a DataFrame by computing various stats for each column.
 
-    It returns a DataFrame with one row per column and the following columns:
+    By default, it returns a DataFrame with one row per column and the following columns
+    (but the columns computed can be customized, see the Customization section below):
 
+    - column_number: Number of the column (useful for sorting)
     - column_name: Name of the column
     - column_type: Type of the column
     - count: Number of rows in the column, it is equal to the number of rows in the table, except for columns nested
@@ -150,8 +188,23 @@ def analyze(df: DataFrame, _chunk_size: int = 50):
     - Arrays are unnested with a `!` character in their name, which is why they may have a different count.
     - Null values are not counted in the count_distinct column.
 
+    Customization:
+    --------------
+    By default, this method will compute for each column the aggregations listed in
+    `bigquery_frame.transformation_impl.analyze.default_aggs`, but users can change this and even add their
+    own custom aggregation by passing the argument `_agg`, a list of aggregation functions with the following signature
+    (col: str, schema_field: SchemaField, col_num: int) -> Column
+
+    Examples of aggregation methods can be found in the module :module:`bigquery_frame.transformation_impl.analyze_aggs`
+
+    Grouping:
+    ---------
+    With the `group_by` option, users can specify one or multiple columns for which the statistics will be grouped.
+    If this option is used, an extra column "group" of type struct will be added to output DataFrame.
+    See the examples below.
+
     Troubleshooting:
-    -----------------------
+    ----------------
     When working on entreprise-level data, BigQuery might return a 400 error with the following message.
     "Not enough resources for query planning - too many subqueries or query is too complex"
 
@@ -164,6 +217,8 @@ def analyze(df: DataFrame, _chunk_size: int = 50):
       If you already persisted the input DataFrame and the same error persist, you can try lowering the value
       of `_chunk_size`, but beware that the lower this value, the slower the analyzis will be.
 
+    Examples:
+    ---------
     >>> df = __get_test_df()
     >>> df.show()
     +----+------------+---------------------+--------------------------------------------+
@@ -179,25 +234,85 @@ def analyze(df: DataFrame, _chunk_size: int = 50):
     |  8 |  Wartortle |           ['Water'] |    {'can_evolve': True, 'evolves_from': 7} |
     |  9 |  Blastoise |           ['Water'] |   {'can_evolve': False, 'evolves_from': 8} |
     +----+------------+---------------------+--------------------------------------------+
-    >>> df = analyze(df)
-    Analyzing 5 columns ...
-    >>> df.withColumn("approx_top_100", f.expr("approx_top_100[OFFSET(0)]"), replace=True).show()  # noqa: E501
-    +------------------------+-------------+-------+----------------+------------+-----------+-----------+------------------------------------+
-    |            column_name | column_type | count | count_distinct | count_null |       min |       max |                     approx_top_100 |
-    +------------------------+-------------+-------+----------------+------------+-----------+-----------+------------------------------------+
-    |                     id |     INTEGER |     9 |              9 |          0 |         1 |         9 |         {'value': '1', 'count': 1} |
-    |                   name |      STRING |     9 |              9 |          0 | Blastoise | Wartortle | {'value': 'Bulbasaur', 'count': 1} |
-    |                 types! |      STRING |    13 |              5 |          0 |      Fire |     Water |     {'value': 'Grass', 'count': 3} |
-    |   evolution.can_evolve |     BOOLEAN |     9 |              2 |          0 |     false |      true |      {'value': 'true', 'count': 6} |
-    | evolution.evolves_from |     INTEGER |     9 |              6 |          3 |         1 |         8 |      {'value': 'NULL', 'count': 3} |
-    +------------------------+-------------+-------+----------------+------------+-----------+-----------+------------------------------------+
 
-    :param df:
-    :param _chunk_size: (default: 50) Try reducing this number if you get this error from BigQuery: "Not enough resources for query planning"
+    >>> analyzed_df = analyze(df)
+    Analyzing 5 columns ...
+    >>> analyzed_df.withColumn("approx_top_100", f.expr("approx_top_100[OFFSET(0)]"), replace=True).show()  # noqa: E501
+    +---------------+------------------------+-------------+-------+----------------+------------+-----------+-----------+------------------------------------+
+    | column_number |            column_name | column_type | count | count_distinct | count_null |       min |       max |                     approx_top_100 |
+    +---------------+------------------------+-------------+-------+----------------+------------+-----------+-----------+------------------------------------+
+    |             0 |                     id |     INTEGER |     9 |              9 |          0 |         1 |         9 |         {'value': '1', 'count': 1} |
+    |             1 |                   name |      STRING |     9 |              9 |          0 | Blastoise | Wartortle | {'value': 'Bulbasaur', 'count': 1} |
+    |             2 |                 types! |      STRING |    13 |              5 |          0 |      Fire |     Water |     {'value': 'Grass', 'count': 3} |
+    |             3 |   evolution.can_evolve |     BOOLEAN |     9 |              2 |          0 |     false |      true |      {'value': 'true', 'count': 6} |
+    |             4 | evolution.evolves_from |     INTEGER |     9 |              6 |          3 |         1 |         8 |      {'value': 'NULL', 'count': 3} |
+    +---------------+------------------------+-------------+-------+----------------+------------+-----------+-----------+------------------------------------+
+
+    >>> df = __get_test_df().withColumn("main_type", f.expr("types[OFFSET(0)]"))
+    >>> df.show()
+    +----+------------+---------------------+--------------------------------------------+-----------+
+    | id |       name |               types |                                  evolution | main_type |
+    +----+------------+---------------------+--------------------------------------------+-----------+
+    |  1 |  Bulbasaur | ['Grass', 'Poison'] | {'can_evolve': True, 'evolves_from': None} |     Grass |
+    |  2 |    Ivysaur | ['Grass', 'Poison'] |    {'can_evolve': True, 'evolves_from': 1} |     Grass |
+    |  3 |   Venusaur | ['Grass', 'Poison'] |   {'can_evolve': False, 'evolves_from': 2} |     Grass |
+    |  4 | Charmander |            ['Fire'] | {'can_evolve': True, 'evolves_from': None} |      Fire |
+    |  5 | Charmeleon |            ['Fire'] |    {'can_evolve': True, 'evolves_from': 4} |      Fire |
+    |  6 |  Charizard |  ['Fire', 'Flying'] |   {'can_evolve': False, 'evolves_from': 5} |      Fire |
+    |  7 |   Squirtle |           ['Water'] | {'can_evolve': True, 'evolves_from': None} |     Water |
+    |  8 |  Wartortle |           ['Water'] |    {'can_evolve': True, 'evolves_from': 7} |     Water |
+    |  9 |  Blastoise |           ['Water'] |   {'can_evolve': False, 'evolves_from': 8} |     Water |
+    +----+------------+---------------------+--------------------------------------------+-----------+
+
+    >>> from bigquery_frame.transformations_impl import analyze_aggs
+    >>> aggs = [
+    ...     analyze_aggs.column_number,
+    ...     analyze_aggs.column_name,
+    ...     analyze_aggs.count,
+    ...     analyze_aggs.count_distinct,
+    ...     analyze_aggs.count_null,
+    ... ]
+    >>> analyzed_df = analyze(df, group_by="main_type", _aggs=aggs)
+    Analyzing 6 columns ...
+    >>> analyzed_df.orderBy("`group`.main_type", "column_number").show()
+    +------------------------+---------------+------------------------+-------+----------------+------------+
+    |                  group | column_number |            column_name | count | count_distinct | count_null |
+    +------------------------+---------------+------------------------+-------+----------------+------------+
+    |  {'main_type': 'Fire'} |             0 |                     id |     3 |              3 |          0 |
+    |  {'main_type': 'Fire'} |             1 |                   name |     3 |              3 |          0 |
+    |  {'main_type': 'Fire'} |             2 |                 types! |     4 |              2 |          0 |
+    |  {'main_type': 'Fire'} |             3 |   evolution.can_evolve |     3 |              2 |          0 |
+    |  {'main_type': 'Fire'} |             4 | evolution.evolves_from |     3 |              2 |          1 |
+    |  {'main_type': 'Fire'} |             5 |              main_type |     3 |              1 |          0 |
+    | {'main_type': 'Grass'} |             0 |                     id |     3 |              3 |          0 |
+    | {'main_type': 'Grass'} |             1 |                   name |     3 |              3 |          0 |
+    | {'main_type': 'Grass'} |             2 |                 types! |     6 |              2 |          0 |
+    | {'main_type': 'Grass'} |             3 |   evolution.can_evolve |     3 |              2 |          0 |
+    | {'main_type': 'Grass'} |             4 | evolution.evolves_from |     3 |              2 |          1 |
+    | {'main_type': 'Grass'} |             5 |              main_type |     3 |              1 |          0 |
+    | {'main_type': 'Water'} |             0 |                     id |     3 |              3 |          0 |
+    | {'main_type': 'Water'} |             1 |                   name |     3 |              3 |          0 |
+    | {'main_type': 'Water'} |             2 |                 types! |     3 |              1 |          0 |
+    | {'main_type': 'Water'} |             3 |   evolution.can_evolve |     3 |              2 |          0 |
+    | {'main_type': 'Water'} |             4 | evolution.evolves_from |     3 |              2 |          1 |
+    | {'main_type': 'Water'} |             5 |              main_type |     3 |              1 |          0 |
+    +------------------------+---------------+------------------------+-------+----------------+------------+
+
+    :param df: a DataFrame
+    :param group_by: (optional) a list of column names on which the aggregations will be grouped
+    :param _aggs: (optional) a list of aggregation to override the default aggregation made by the function
+    :param _chunk_size: (default: 50) Try reducing this number if you get this error from BigQuery:
+        "Not enough resources for query planning"
     :return:
     """
+    if group_by is None:
+        group_by = []
+    if _aggs is None:
+        _aggs = default_aggs
+    if isinstance(group_by, str):
+        group_by = [group_by]
     flat_schema = flatten_schema(df.schema, explode=True)
-    col_dfs = [_analyze_column(df, schema_field, num) for num, schema_field in enumerate(flat_schema)]
+    col_dfs = [_analyze_column(df, schema_field, num, group_by, _aggs) for num, schema_field in enumerate(flat_schema)]
 
     nb_cols = len(col_dfs)
     print(f"Analyzing {nb_cols} columns ...")
@@ -210,7 +325,7 @@ def analyze(df: DataFrame, _chunk_size: int = 50):
         union_df = union_dataframes(big_dfs)
     # For some reason, `union_df.orderBy("column_number").drop("column_number")`
     # does not preserve the ordering, but this does:
-    query = f"""SELECT * EXCEPT(column_number) FROM {quote(union_df._alias)} ORDER BY column_number"""
+    query = f"""SELECT * FROM {quote(union_df._alias)} ORDER BY column_number"""
     return union_df._apply_query(query)
 
 
--- tests/transformations_impl/test_analyze.py
+++ tests/transformations_impl/test_analyze.py
@@ -23,11 +23,11 @@ field_to_index = {
 def get_expected() -> List[Row]:
     # fmt: off
     expected = [
-            Row(('id', 'INTEGER', 9, 9, 0, '1', '9', [{'value': '1', 'count': 1}, {'value': '2', 'count': 1}, {'value': '3', 'count': 1}, {'value': '4', 'count': 1}, {'value': '5', 'count': 1}, {'value': '6', 'count': 1}, {'value': '7', 'count': 1}, {'value': '8', 'count': 1}, {'value': '9', 'count': 1}]), field_to_index),  # noqa: E501
-            Row(('name', 'STRING', 9, 9, 0, 'Blastoise', 'Wartortle', [{'value': 'Bulbasaur', 'count': 1}, {'value': 'Ivysaur', 'count': 1}, {'value': 'Venusaur', 'count': 1}, {'value': 'Charmander', 'count': 1}, {'value': 'Charmeleon', 'count': 1}, {'value': 'Charizard', 'count': 1}, {'value': 'Squirtle', 'count': 1}, {'value': 'Wartortle', 'count': 1}, {'value': 'Blastoise', 'count': 1}]), field_to_index),  # noqa: E501
-            Row(('types!', 'STRING', 13, 5, 0, 'Fire', 'Water', [{'value': 'Grass', 'count': 3}, {'value': 'Poison', 'count': 3}, {'value': 'Fire', 'count': 3}, {'value': 'Water', 'count': 3}, {'value': 'Flying', 'count': 1}]), field_to_index),  # noqa: E501
-            Row(('evolution.can_evolve', 'BOOLEAN', 9, 2, 0, 'false', 'true', [{'value': 'true', 'count': 6}, {'value': 'false', 'count': 3}]), field_to_index),  # noqa: E501
-            Row(('evolution.evolves_from', 'INTEGER', 9, 6, 3, '1', '8', [{'value': 'NULL', 'count': 3}, {'value': '1', 'count': 1}, {'value': '2', 'count': 1}, {'value': '4', 'count': 1}, {'value': '5', 'count': 1}, {'value': '7', 'count': 1}, {'value': '8', 'count': 1}]), field_to_index)  # noqa: E501
+        Row((0, 'id', 'INTEGER', 9, 9, 0, '1', '9', [{'value': '1', 'count': 1}, {'value': '2', 'count': 1}, {'value': '3', 'count': 1}, {'value': '4', 'count': 1}, {'value': '5', 'count': 1}, {'value': '6', 'count': 1}, {'value': '7', 'count': 1}, {'value': '8', 'count': 1}, {'value': '9', 'count': 1}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8}),  # noqa: E501
+        Row((1, 'name', 'STRING', 9, 9, 0, 'Blastoise', 'Wartortle', [{'value': 'Bulbasaur', 'count': 1}, {'value': 'Ivysaur', 'count': 1}, {'value': 'Venusaur', 'count': 1}, {'value': 'Charmander', 'count': 1}, {'value': 'Charmeleon', 'count': 1}, {'value': 'Charizard', 'count': 1}, {'value': 'Squirtle', 'count': 1}, {'value': 'Wartortle', 'count': 1}, {'value': 'Blastoise', 'count': 1}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8}),  # noqa: E501
+        Row((2, 'types!', 'STRING', 13, 5, 0, 'Fire', 'Water', [{'value': 'Grass', 'count': 3}, {'value': 'Poison', 'count': 3}, {'value': 'Fire', 'count': 3}, {'value': 'Water', 'count': 3}, {'value': 'Flying', 'count': 1}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8}),  # noqa: E501
+        Row((3, 'evolution.can_evolve', 'BOOLEAN', 9, 2, 0, 'false', 'true', [{'value': 'true', 'count': 6}, {'value': 'false', 'count': 3}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8}),  # noqa: E501
+        Row((4, 'evolution.evolves_from', 'INTEGER', 9, 6, 3, '1', '8', [{'value': 'NULL', 'count': 3}, {'value': '1', 'count': 1}, {'value': '2', 'count': 1}, {'value': '4', 'count': 1}, {'value': '5', 'count': 1}, {'value': '7', 'count': 1}, {'value': '8', 'count': 1}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8})  # noqa: E501
     ]
     # fmt: on
     return expected
@@ -54,8 +54,8 @@ class TestAnalyze(unittest.TestCase):
         actual = analyze(df)
         # fmt: off
         expected = [
-            Row(('FROM', 'INTEGER', 1, 1, 0, '1', '1', [{'value': '1', 'count': 1}]), field_to_index),
-            Row(('UNION.ALL', 'STRING', 1, 1, 0, 'a', 'a', [{'value': 'a', 'count': 1}]), field_to_index),
+            Row((0, 'FROM', 'INTEGER', 1, 1, 0, '1', '1', [{'value': '1', 'count': 1}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8}),  # noqa: E501
+            Row((1, 'UNION.ALL', 'STRING', 1, 1, 0, 'a', 'a', [{'value': 'a', 'count': 1}]), {'column_number': 0, 'column_name': 1, 'column_type': 2, 'count': 3, 'count_distinct': 4, 'count_null': 5, 'min': 6, 'max': 7, 'approx_top_100': 8})  # noqa: E501
         ]
         # fmt: on
         self.assertEqual(expected, actual.collect())
