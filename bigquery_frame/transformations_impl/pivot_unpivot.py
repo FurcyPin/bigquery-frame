0d4f7784bb99e3c65032eb2376c74f14780e34d0
Furcy Pin <pin.furcy@gmail.com>
2023-12-22 22:47:28 +0100

    feat!: `transformations.pivot` now support doing multiple aggregations at the same time
    The method signature has changed,
    and mplementation v1 has been dropped.

--- bigquery_frame/transformations_impl/pivot_unpivot.py
+++ bigquery_frame/transformations_impl/pivot_unpivot.py
@@ -1,26 +1,23 @@
 from typing import List
 
 from bigquery_frame import BigQueryBuilder, DataFrame
-from bigquery_frame.column import cols_to_str
+from bigquery_frame.column import StringOrColumn, cols_to_str
 from bigquery_frame.dataframe import strip_margin
 from bigquery_frame.utils import quote
 
 
-def pivot(
-    df: DataFrame,
-    pivot_column: str,
-    agg_fun: str,
-    agg_col: str,
-    pivoted_columns: List[str] = None,
-    implem_version: int = 1,
-) -> DataFrame:
+def pivot(df: DataFrame, pivot_column: str, aggs: List[StringOrColumn], pivoted_columns: List[str] = None) -> DataFrame:
     """Pivots a column of the current :class:`DataFrame` and performs the specified aggregation.
     There are two versions of pivot function: one that requires the caller to specify the list
     of distinct values to pivot on, and one that does not. The latter is more concise but less
     efficient, because we need to first compute the list of distinct values internally.
 
+    This uses BigQuery's
+    `PIVOT operator <https://cloud.google.com/bigquery/docs/reference/standard-sql/query-syntax#pivot_operator>`_
+
     Example:
     >>> from bigquery_frame import BigQueryBuilder
+    >>> from bigquery_frame import functions as f
     >>> bq = BigQueryBuilder()
     >>> df = __get_test_unpivoted_df(bq)
     >>> df.show()
@@ -49,7 +46,7 @@ def pivot(
     | 2019 |  Banana |   China |   1400 |
     +------+---------+---------+--------+
     only showing top 20 rows
-    >>> pivot(df, pivot_column="country", agg_fun="sum", agg_col="amount").show()
+    >>> pivot(df, pivot_column="country", aggs=["sum(amount)"]).show()
     +------+---------+--------+-------+--------+
     | year | product | Canada | China | Mexico |
     +------+---------+--------+-------+--------+
@@ -62,64 +59,30 @@ def pivot(
     | 2019 |  Banana |   null |  1400 |    400 |
     | 2019 | Carrots |   null |   200 |   null |
     +------+---------+--------+-------+--------+
+    >>> pivot(
+    ...     df, 
+    ...     pivot_column="country", 
+    ...     aggs=["sum(amount) as total_amount", f.count(f.col("year")).alias("nb_years")]
+    ... ).show()
+    +---------+---------------------+-----------------+--------------------+----------------+---------------------+-----------------+
+    | product | total_amount_Canada | nb_years_Canada | total_amount_China | nb_years_China | total_amount_Mexico | nb_years_Mexico |
+    +---------+---------------------+-----------------+--------------------+----------------+---------------------+-----------------+
+    |  Orange |                5000 |               2 |               4000 |              2 |                5000 |               2 |
+    |   Beans |                null |               2 |               3000 |              2 |                4000 |               2 |
+    |  Banana |                2000 |               2 |               1800 |              2 |                 400 |               2 |
+    | Carrots |                2000 |               2 |               1400 |              2 |                null |               2 |
+    +---------+---------------------+-----------------+--------------------+----------------+---------------------+-----------------+
 
     :param df: a DataFrame
     :param pivot_column: column to pivot
-    :param agg_fun: aggregation function that will be applied
-    :param agg_col: column that will be aggregated
+    :param aggs: aggregations that will be applied.
+                 If more than one aggregations are passed, they must be aliased
     :param pivoted_columns: (Optional) list of distinct values in the pivot column.
         Execution will be faster if provided.
-    :param implem_version: (Possible values [1, 2]) Version of the code to use.
-        Version 2 uses the BigQuery's PIVOT statement, while version 1 doesn't.
     :return:
-    """
-    if implem_version == 1:
-        return pivot_v1(df, pivot_column, agg_fun, agg_col, pivoted_columns)
-    elif implem_version == 2:
-        return pivot_v2(df, pivot_column, agg_fun, agg_col, pivoted_columns)
-    else:
-        raise Exception("Pivot, please use v1 or v2")
-
-
-def pivot_v1(
-    df: DataFrame,
-    pivot_column: str,
-    agg_fun: str,
-    agg_col: str,
-    pivoted_columns: List[str] = None,
-) -> DataFrame:
-    """This version uses a good old GROUP BY statement and should be compatible with most ANSI-SQL engines."""
-    group_columns = [col for col in df.columns if col.lower() not in [agg_col.lower(), pivot_column.lower()]]
-    distinct_query = f"""SELECT DISTINCT {pivot_column} FROM {quote(df._alias)}"""
+    """  # noqa: E501
     if pivoted_columns is None:
-        pivoted_columns = [row.get(pivot_column) for row in df._apply_query(distinct_query).collect()]
-
-    aggregates = [f"""{agg_fun}(IF({pivot_column} = '{col}', {agg_col}, null)) as {col}""" for col in pivoted_columns]
-
-    group_query = strip_margin(
-        f"""
-        |SELECT
-        |{cols_to_str(group_columns, 2)},
-        |{cols_to_str(aggregates, 2)}
-        |FROM {quote(df._alias)}
-        |GROUP BY {cols_to_str(group_columns)}
-        |"""
-    )
-    return df._apply_query(group_query)
-
-
-def pivot_v2(
-    df: DataFrame,
-    pivot_column: str,
-    agg_fun: str,
-    agg_col: str,
-    pivoted_columns: List[str] = None,
-) -> DataFrame:
-    """This version uses BigQuery's
-    `PIVOT operator <https://cloud.google.com/bigquery/docs/reference/standard-sql/query-syntax#pivot_operator>`_
-    """
-    distinct_query = f"""SELECT DISTINCT {pivot_column} FROM {quote(df._alias)}"""
-    if pivoted_columns is None:
+        distinct_query = f"""SELECT DISTINCT {pivot_column} FROM {quote(df._alias)}"""
         pivoted_columns = [row.get(pivot_column) for row in df._apply_query(distinct_query).collect()]
     quoted_pivoted_columns = [f"'{col}'" for col in pivoted_columns]
 
@@ -128,7 +91,7 @@ def pivot_v2(
         |SELECT
         | *
         |FROM {quote(df._alias)}
-        |PIVOT({agg_fun}({agg_col}) FOR {pivot_column} IN ({cols_to_str(quoted_pivoted_columns)}))
+        |PIVOT({cols_to_str(aggs)} FOR {pivot_column} IN ({cols_to_str(quoted_pivoted_columns)}))
         |"""
     )
     return df._apply_query(group_query)
