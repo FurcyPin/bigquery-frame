10a167d9606c36085784630eeb42e5d8c110f079
Furcy Pin <pin.furcy@gmail.com>
2023-12-28 10:04:42 +0100

    feat: add nested.unnest_field and nested.unnest_all_fields

--- bigquery_frame/nested.py
+++ bigquery_frame/nested.py
@@ -2,6 +2,8 @@ from bigquery_frame.nested_impl.fields import fields
 from bigquery_frame.nested_impl.print_schema import print_schema
 from bigquery_frame.nested_impl.schema_string import schema_string
 from bigquery_frame.nested_impl.select_impl import select
+from bigquery_frame.nested_impl.unnest_all_fields import unnest_all_fields
+from bigquery_frame.nested_impl.unnest_field import unnest_field
 from bigquery_frame.nested_impl.with_fields import with_fields
 from bigquery_frame.utils import _ref
 
@@ -9,4 +11,6 @@ _ref(fields)
 _ref(print_schema)
 _ref(schema_string)
 _ref(select)
+_ref(unnest_all_fields)
+_ref(unnest_field)
 _ref(with_fields)
